de402cf1c77b3296e217608e3482af64a2774fe0
Visal Chea <visal_chea@phillipbank.com.kh>
2020-03-14 13:28:55 +0700

    init 2

---
+++ bin/Debug/query.sql
@@ -0,0 +1,5 @@
+--select * from dim_branch_v
+----select top 30 * FROM Employee e
+--select * from workorder limit 25
+
+SELECT * FROM RPT_RC_ACTIVE_DLY_V A WHERE A.REPORT_DATE = &P_DATE
---
+++ bin/Release/query.sql
@@ -0,0 +1,3 @@
+select * from tmp_loan_test01 where xrownum <= 200
+--select seq seq_no, t.* from tmp_casa_test01 t where rownum <= 15
+--select * from  rpt_casa_active_dly_krd_v r where r.REPORT_DATE = '30 Nov 19'
